c0145a220cbad593d93e66cf1082f910327d4069
NK <0ht53152387219x@gmail.com>
2025-08-07 15:11:38 +0900

    first commit

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.41.0"
+    }
+  }
+  cloud{
+    organization = "AWS-practice-kazu-man"
+    workspaces {
+      name = "aws-infra"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_subnet" "test_subnet" {
+  vpc_id                  = "vpc-f2595d95"
+  cidr_block              = "172.31.1.0/24"
+  availability_zone       = "ap-northeast-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "TestSubnet"
+  }
+}
+
+resource "aws_instance" "test_server" {
+  ami           = "ami-0eba6c58b7918d3a1"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.test_subnet.id
+
+  tags = {
+    Name = "TestInstance"
+  }
+}
